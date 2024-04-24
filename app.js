a49756021cb4ebc35782503088fea2faa7e6ea78
Muhammad Yameen <muhammadyameen5432@gmail.com>
2024-04-24 11:09:33 +0500

    feat: create a simple api endpoint the convert the ppt file to pdf file

---
+++ app.js
@@ -0,0 +1,42 @@
+import fs from "fs";
+import express from "express";
+import FormData from "form-data"
+import axios from "axios";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express();
+
+
+const formData = new FormData()
+formData.append('instructions', JSON.stringify({
+    parts: [
+        {
+            file: "file"
+        }
+    ]
+}))
+
+formData.append('file', fs.createReadStream('testing.ppt'))
+
+app.post("/convert", async (req, res) => {
+    try {
+        const response = await axios.post('https://api.pspdfkit.com/build', formData, {
+            headers: formData.getHeaders({
+                'Authorization': `Bearer ${process.env.API_KEY} `
+            }),
+            responseType: "stream"
+        })
+
+        const data = response.data.pipe(fs.createWriteStream("result.pdf"))
+        res.send({ message: "Converted Sucessfully", data: data })
+    } catch (e) {
+        console.log("🚀 ~ app.post ~ e:", e)
+        res.send({ message: "Error Occured", data: e })
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Server is Started");
+});
